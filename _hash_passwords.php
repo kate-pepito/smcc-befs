4da1f46dabd1ecdc6ab39744f675e40f4201023b
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2025-02-20 18:42:53 +0800

    added patch hashed password

--- _hash_passwords.php
+++ _hash_passwords.php
@@ -2,12 +2,21 @@
 
 require_once('dbconnect.php');
 
+function saveToTxt($data) {
+    $txt = "";
+    foreach ($data as $k => $v) {
+        foreach ($v as $d)
+            $txt .= "[$k]: " . $d['username'] . " = " . $d['old_password'] . PHP_EOL;
+    }
+    file_put_contents("_passwords.txt", $txt);
+}
+
 $result = [
     "users" => [],
     "students" => []
 ];
 
-$q = "SELECT id, password from users";
+$q = "SELECT id, username, password from users";
 
 $query = mysqli_query($conn, $q);
 
@@ -19,6 +28,7 @@ if (mysqli_num_rows($query) > 0) {
             if (mysqli_query($conn, "UPDATE users SET password = '$newPassword' WHERE id = $id")) {
                 $result["users"][] = [
                     "id" => $id,
+                    "username" => $row['username'],
                     "old_password" => $row['password'],
                     "new_password" => $newPassword
                 ];
@@ -28,7 +38,7 @@ if (mysqli_num_rows($query) > 0) {
 }
 
 
-$q = "SELECT id, password from students";
+$q = "SELECT id, username, password from students";
 
 $query = mysqli_query($conn, $q);
 
@@ -40,6 +50,7 @@ if (mysqli_num_rows($query) > 0) {
             if (mysqli_query($conn, "UPDATE students SET password = '$newPassword' WHERE id = $id")) {
                 $result["students"][] = [
                     "id" => $id,
+                    "username" => $row['username'],
                     "old_password" => $row['password'],
                     "new_password" => $newPassword
                 ];
@@ -48,5 +59,6 @@ if (mysqli_num_rows($query) > 0) {
     }
 }
 
+saveToTxt($result);
 header("Content-Type: application/json");
 die(json_encode($result));
